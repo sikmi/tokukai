ce4a7fec1a0fdb34f190ad5dd5b8a525a5d0cfb1
yoshitsugu <yoshitsugu@sikmi.com>
2020-12-23 10:35:51 +0900

    Add mulOfSum3

--- 20201221/yoshitsugu_hs/day1.hs
+++ 20201221/yoshitsugu_hs/day1.hs
@@ -8,9 +8,22 @@ mulOfSum n xs =
         ((f, s):_) -> Just $ f * s
         _ -> Nothing
 
+mulOfSum3 :: Int -> [Int] -> Maybe Int
+mulOfSum3 n [] = Nothing
+mulOfSum3 n xs =
+    let r = [(i, j, k) | 
+              i <- xs,
+              j <- (filter (\x -> x /= i) xs),
+              k <- (filter (\x -> x /= i && x /= j) xs),
+              i + j + k == n]
+    in case r of 
+        ((f, s, t):_) -> Just $ f * s * t
+        _ -> Nothing
+
 main :: IO()
 main = do
     input <- readFile "day1.txt"
     let numbers = map (\x -> read x :: Int) $ lines input
     putStrLn . show $ mulOfSum 2020 numbers
+    putStrLn. show $ mulOfSum3 2020 numbers
     
