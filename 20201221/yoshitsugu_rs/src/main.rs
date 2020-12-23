e31f8d1a5582c5e4af6a59b9d074fe8c01f38f51
yoshitsugu <yoshitsugu@sikmi.com>
2020-12-23 10:47:12 +0900

    Add mul_of_sum3 to yoshitsugu_rs

--- 20201221/yoshitsugu_rs/src/main.rs
+++ 20201221/yoshitsugu_rs/src/main.rs
@@ -24,8 +24,25 @@ fn mul_of_sum(s: usize, n: usize, ns: &[usize], previous: Option<usize>) -> Opti
     }
 }
 
+fn mul_of_sum3(s: usize, n: usize, ns: &[usize]) -> Option<usize> {
+    let s2 = s - n;
+    let mut remains = ns[1..].to_vec();
+    for _ in 1..ns.len() - 1 {
+        if remains.len() == 0 {
+            break;
+        }
+        let number = remains[0];
+        let slice = remains[1..].to_vec();
+        if let Some(r) = mul_of_sum(s2, number, &slice, None) {
+            return Some(r * n);
+        }
+        remains.remove(0);
+    }
+    return None;
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let content: String = fs::read_to_string("../ynishi/day1.data")?;
+    let content: String = fs::read_to_string("../yoshitsugu_hs/day1.txt")?;
     let mut numbers = content
         .split("\n")
         .filter_map(|s| usize::from_str(s).ok())
@@ -46,5 +63,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         remains.remove(0);
     }
     println!("{:?}", result);
+    let mut result3: Option<usize> = None;
+    let mut remains3 = numbers[1..].to_vec();
+    for _ in 1..numbers.len() - 1 {
+        if remains3.len() == 0 {
+            break;
+        }
+        let number = remains3[0];
+        let slice = remains3[1..].to_vec();
+        result3 = mul_of_sum3(2020, number, &slice);
+        if result3.is_some() {
+            break;
+        }
+        remains3.remove(0);
+    }
+    println!("{:?}", result3);
     Ok(())
 }
