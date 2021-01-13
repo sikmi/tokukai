be7679a7bcaf276d86113ecc250d73cfefc70931
HiroyaSakamoto <sakamoto@sikmi.com>
2021-01-13 17:37:38 +0900

    コメント

--- 20210113/sakamoto/day5.rb
+++ 20210113/sakamoto/day5.rb
@@ -4,11 +4,15 @@ cols = [*0..7]
 ids = []
 
 File.open(File.dirname(__FILE__) + '/day5.txt') do |f|
+  # １行毎を要素として配列化
   lines = f.readlines.map {|l| l.chop}
   lines.each do|line|
+    # 初期値のセット
     thisrows = rows
     thiscols = cols
+    # 1文字ずつに分割
     seats = line.split("")
+    #各文字の値に応じてrow,colを分割し、最終的な座席を特定
     seats.each do |seat|
       case seat
       when "F" then
@@ -20,8 +24,10 @@ File.open(File.dirname(__FILE__) + '/day5.txt') do |f|
       when "R" then
         thiscols = thiscols.slice((thiscols.size/2)..thiscols.size)
       end
+      # 特定した座席からIDを計算し、代入
       ids << thisrows[0] * 8 + thiscols[0]
     end
   end
+  # 最大値を表示
   p ids.max
 end
