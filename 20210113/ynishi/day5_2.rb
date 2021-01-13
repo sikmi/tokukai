d03fbcb3ff30e1281ec1f61b6d965ca529eb6b85
pocari <caffelattenonsugar@gmail.com>
2021-01-13 18:56:05 +0900

    不要なダンプ削除

--- 20210113/ynishi/day5_2.rb
+++ 20210113/ynishi/day5_2.rb
@@ -101,11 +101,10 @@ def main
 
   #候補(candidates)のうち、前後(idの+1, -1)がfalse(== リストにいた)ものを探す
   result = candidates.each_cons(3).find_all do |(id1, flag1), (id2, flag2), (id3, flag3)|
-    p [id1, id2, id3]
     !flag1 && flag2 && !flag3
   end
 
-  # 候補マップを表示
+  # 候補マップを表示(xが候補)
   # board = (0 .. 127).map do |r|
   #   (0 .. 7).map do |c|
   #     val = case candidates[r * 8 + c]
