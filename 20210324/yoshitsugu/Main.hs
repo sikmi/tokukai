54d21f1905a9e2e40524ad85a22dcc78cac0ea7f
yoshitsugu <yoshitsugu@sikmi.com>
2021-03-25 19:36:01 +0900

    Add yoshitsugu day14-2

--- 20210324/yoshitsugu/Main.hs
+++ 20210324/yoshitsugu/Main.hs
@@ -17,6 +17,7 @@ import qualified Data.ByteString.Char8 as C
 import Data.Char (digitToInt, isSpace)
 import Data.Coerce (coerce)
 import Data.Foldable (asum)
+import qualified Data.IntMap.Strict as IS
 import Data.List (scanl', sortOn)
 import Data.Maybe (fromJust, isJust, listToMaybe)
 import Data.STRef
@@ -29,6 +30,8 @@ import qualified Text.Parsec as P
 import Text.Parsec.String (Parser)
 
 data Command = Mask (Int, Int) | Mem (Int, Int) deriving (Show)
+
+data Command2 = Mask2 String | Mem2 (Int, Int) deriving (Show)
 
 readBin :: Integral a => String -> Maybe a
 readBin = fmap fst . listToMaybe . readInt 2 (`elem` "01") digitToInt
@@ -68,11 +71,56 @@ solve n = solve' (U.replicate (n + 1) 0) (0, 0)
       let nv = andFilter .&. (orFilter .|. v)
        in solve' (U.update ms $ U.fromList [(m, nv)]) f rcs
 
+maskParser2 :: Parser Command2
+maskParser2 = do
+  P.string "mask = "
+  value <- P.many1 $ P.choice [P.char 'X', P.char '1', P.char '0']
+  return $ Mask2 value
+
+memParser2 :: Parser Command2
+memParser2 = do
+  P.string "mem["
+  addr <- P.many1 P.digit
+  P.string "] = "
+  value <- P.many1 P.digit
+  return $ Mem2 (read addr, read value)
+
+lineParser2 :: Parser Command2
+lineParser2 = P.try maskParser2 <|> memParser2
+
+parseLine2 :: String -> Command2
+parseLine2 s =
+  case P.runParser lineParser2 () "parser" s of
+    Right s -> s
+    Left e -> error (show e ++ show s)
+
+solve2 :: [Command2] -> Int
+solve2 = solve' IS.empty ""
+  where
+    solve' :: IS.IntMap Int -> String -> [Command2] -> Int
+    solve' ms _ [] = sum . map snd $ IS.toList ms
+    solve' ms f ((Mask2 f') : rcs) = solve' ms f' rcs
+    solve' ms f ((Mem2 (m, v)) : rcs) =
+      let ms' =
+            U.foldl'
+              (\ms_ addr -> IS.insert addr v ms_)
+              ms
+              (addrs 1 (reverse f) m)
+       in solve' ms' f rcs
+      where
+        addrs :: Int -> String -> Int -> U.Vector Int
+        addrs _ [] _ = U.singleton 0
+        addrs n ('1' : cs) m = U.map (+ n) (addrs (n * 2) cs m)
+        addrs n ('0' : cs) m = U.map (\a -> if n .&. m > 0 then a + n else a) (addrs (n * 2) cs m)
+        addrs n ('X' : cs) m = U.map (+ n) (addrs (n * 2) cs m) U.++ addrs (n * 2) cs m
+
 main :: IO ()
 main = do
   contents <- getContents
   let commands = map parseLine $ lines contents
   print $ solve (maxMemNum commands) commands
+  let command2 = map parseLine2 $ lines contents
+  print $ solve2 command2
   where
     maxMemNum :: [Command] -> Int
     maxMemNum [] = 0
