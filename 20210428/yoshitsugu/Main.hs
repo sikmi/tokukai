c8449e89a22db32155206ab30fd95a3d1d0fe460
yoshitsugu <yoshitsugu@sikmi.com>
2021-04-30 18:07:07 +0900

    Fix yoshitsugu day18 using megaparsec

--- 20210428/yoshitsugu/Main.hs
+++ 20210428/yoshitsugu/Main.hs
@@ -1,114 +1,93 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE Strict #-}
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeApplications #-}
 
 import Control.Monad
-import Control.Monad.ST
-import qualified Data.HashMap.Strict as H
-import Data.List (isPrefixOf)
-import Data.List.Split (splitOn, startsWith)
-import Data.STRef
-import Debug.Trace
-import qualified Text.Parsec as P
-import Text.Parsec.Char
-import Text.Parsec.String (Parser)
-import qualified Text.Parsec.Token as PT
 import Data.Functor.Identity (Identity)
+import Data.Text (Text)
+import qualified Data.Text as T
+import Data.Void (Void)
+import Debug.Trace
+import Text.Megaparsec hiding (State)
+import qualified Text.Megaparsec as M
+import Text.Megaparsec.Char hiding (char)
+import qualified Text.Megaparsec.Char.Lexer as L
 
-data Expr = Mul Expr Expr | Pls Expr Expr | Val Int deriving (Show, Eq)
+data Expr = Mul Expr Expr | Add Expr Expr | Val Int deriving (Show, Eq)
 
-primary :: Parser Expr
-primary = do
-  P.spaces
-  b <- P.string "(" P.<|> P.many1 digit
-  P.spaces
-  if b == "("
-    then do
-      e <- expr
-      P.char ')'
-      P.spaces
-      return e
-    else
-      return . Val $ read b
+type Parser = Parsec Void Text
 
-mul :: P.ParsecT String u Identity (Expr -> Expr -> Expr)
-mul = do
-  P.spaces 
-  P.string "*"
-  return Mul
+sc :: Parser ()
+sc =
+  L.space
+    space1
+    (L.skipLineComment "//")
+    (L.skipBlockComment "/*" "*/")
 
-pls :: P.ParsecT String u Identity (Expr -> Expr -> Expr)
-pls = do
-  P.spaces   
-  P.string "+"
-  return Pls
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme sc
 
+symbol :: Text -> Parser Text
+symbol = L.symbol sc
 
-term :: Parser Expr
-term = P.chainl1 primary (mul P.<|> pls)
+primary :: Int -> Parser Expr
+primary n =
+  do
+    number <- lexeme L.decimal
+    return $ Val number
+    <|> between (symbol "(") (symbol ")") (expr n)
 
-expr:: Parser Expr
+chainl1 :: Parser Expr -> Parser (Expr -> Expr -> Expr) -> Parser Expr
+chainl1 p op = do
+  x <- p
+  rest x
+  where
+    rest x =
+      do
+        f <- op
+        y <- p
+        rest (f x y)
+        <|> return x
+
+muladd :: Int -> Parser Expr
+muladd n = chainl1 (primary n) $ (Mul <$ symbol "*") <|> (Add <$ symbol "+")
+
+mul :: Int -> Parser Expr
+mul n = chainl1 (add n) (Mul <$ symbol "*")
+
+add :: Int -> Parser Expr
+add n = chainl1 (primary n) (Add <$ symbol "+")
+
+term :: Int -> Parser Expr
+term n = if n == 1 then muladd 1 else mul 2
+
+expr :: Int -> Parser Expr
 expr = term
 
-inputParser :: Parser [Expr]
-inputParser = do
-  P.many1 expr
-
-parseInput1 :: String -> [Expr]
-parseInput1 s =
-  case P.runParser inputParser () "parser" s of
+parseInput :: Int -> Text -> Expr
+parseInput n input =
+  case parse (expr n) "" input of
     Right s -> s
-    Left e -> error (show e ++ " --- " ++ show s)
-
-primary2 :: Parser Expr
-primary2 = do
-  P.spaces
-  b <- P.string "(" P.<|> P.many1 digit
-  P.spaces
-  if b == "("
-    then do
-      e <- expr2
-      P.char ')'
-      P.spaces
-      return e
-    else
-      return . Val $ read b
-
-ps :: Parser Expr
-ps = P.chainl1 primary2 pls
-
-term2 :: Parser Expr
-term2 = P.chainl1 ps mul
-
-expr2:: Parser Expr
-expr2 = term2
-
-parseInput2 :: String -> [Expr]
-parseInput2 s =
-  case P.runParser (P.many1 expr2) () "parser" s of
-    Right s -> s
-    Left e -> error (show e ++ " --- " ++ show s)
+    Left e -> error $ "PARSE ERROR: " ++ errorBundlePretty e
 
 solve :: [Expr] -> Int
 solve [] = 0
-solve (expr:exprs) = eval expr + solve exprs
+solve (expr : exprs) = eval expr + solve exprs
   where
     eval :: Expr -> Int
-    eval (Mul x y) =
-      let ex = eval x
-       in ex * eval y
-    eval (Pls x y) =
-      let ex = eval x
-       in ex + eval y
+    eval (Mul x y) = eval x * eval y
+    eval (Add x y) = eval x + eval y
     eval (Val n) = n
 
 main :: IO ()
 main = do
-  contents <- getContents  
-  let exprs1 = parseInput1 contents
+  contents <- lines <$> getContents
+  let exprs1 = map (parseInput 1 . T.pack) contents
   print $ solve exprs1
-  let exprs2 = parseInput2 contents
+  let exprs2 = map (parseInput 2 . T.pack) contents
   print $ solve exprs2
