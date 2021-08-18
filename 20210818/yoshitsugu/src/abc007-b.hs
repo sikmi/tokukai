6c464d0c5ddacd278d18157790ab92d4d6060bf1
yoshitsugu <yoshitsugu@sikmi.com>
2021-08-18 17:58:55 +0900

    yoshitsugu 20210818

---
+++ 20210818/yoshitsugu/src/abc007-b.hs
@@ -0,0 +1,68 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE Strict #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE TypeApplications #-}
+{-# OPTIONS_GHC -O2 #-}
+
+import Control.Applicative
+import Control.Arrow (second)
+import Control.Monad.ST (ST, runST)
+import Control.Monad.State.Strict
+import qualified Data.Bits as BT
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as C
+import Data.Char (isSpace, ord)
+import Data.Coerce (coerce)
+-- import qualified Data.HashMap.Strict as HS
+import Data.IORef (IORef, modifyIORef', newIORef, readIORef)
+import Data.List (foldl')
+import Data.Maybe (fromJust, fromMaybe)
+import Data.STRef (STRef)
+import Data.STRef.Strict (modifySTRef', newSTRef, readSTRef, writeSTRef)
+import qualified Data.Vector as V
+import qualified Data.Vector.Mutable as VM
+import qualified Data.Vector.Unboxed as U
+import qualified Data.Vector.Unboxed.Mutable as UM
+import Data.Word (Word8)
+import Debug.Trace
+
+main :: IO ()
+main = do
+  cs <- getLine
+  if cs == "a"
+    then putStrLn "-1"
+    else putStrLn "a"
+
+readIntTuple :: C.ByteString -> (Int, Int)
+readIntTuple b = (\v -> (v U.! 0, v U.! 1)) (U.unfoldrN 2 (runParser int) b)
+
+readIntTriple :: C.ByteString -> (Int, Int, Int)
+readIntTriple b = (\v -> (v U.! 0, v U.! 1, v U.! 2)) (U.unfoldrN 3 (runParser int) b)
+
+type Parser a = StateT C.ByteString Maybe a
+
+runParser :: Parser a -> C.ByteString -> Maybe (a, C.ByteString)
+runParser = runStateT
+{-# INLINE runParser #-}
+
+int :: Parser Int
+int = coerce $ C.readInt . C.dropWhile isSpace
+{-# INLINE int #-}
+
+int1 :: Parser Int
+int1 = fmap (subtract 1) int
+{-# INLINE int1 #-}
+
+char :: Parser Char
+char = coerce C.uncons
+{-# INLINE char #-}
+
+byte :: Parser Word8
+byte = coerce B.uncons
+{-# INLINE byte #-}
+
+skipSpaces :: Parser ()
+skipSpaces = modify' (C.dropWhile isSpace)
+{-# INLINE skipSpaces #-}
