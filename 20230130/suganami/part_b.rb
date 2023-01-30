2372f68e03d440bfafb7e4fb72d22a44eaa08474
Ryosuke Suganami <*****@users.noreply.github.com>
2023-01-30 18:23:05 +0900

    コメント追記

--- 20230130/suganami/part_b.rb
+++ 20230130/suganami/part_b.rb
@@ -12,12 +12,13 @@ def main
   test(1, 'A111111B', 'Yes')
   test(4, 'A100000B', 'Yes')
   test(5, 'A999999B', 'Yes')
-  test(2, 'A011111B', 'No')
-  test(3, 'AB111111B', 'No')
-  test(6, 'A11111B', 'No')
-  test(7, 'A111111BC', 'No')
-  test(8, 'A1111111BC', 'No')
-  test(9, 'X900000', 'No')
+  test(2, 'A011111B', 'No') # 100000〜999999に入らない
+  test(3, 'AB111111B', 'No') # 前2つNG
+  test(6, 'A11111B', 'No') # 5桁NG
+  test(7, 'A111111BC', 'No') # 後ろ2つNG
+  test(8, 'A1111111BC', 'No') # 後ろ2つ + 7桁 NG
+  test(9, 'X900000', 'No') # 後ろのアルファベットがない NG
+  test(7, 'A1111111B', 'No') # 7桁NG
 end
 
 def test(no, string, expected)
